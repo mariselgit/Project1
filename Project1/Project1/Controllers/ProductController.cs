c84974eb5f034605b459a4caa56c7b28d0b144d0
Marisel Bueno Rojas <marisel.rojas@jenkon.com>
2019-09-17 10:24:09 -0700

    Products

---
+++ Project1/Project1/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            return View(new Product(){ Id = 1, Name = "Product 1"});
+        }
+
+    }
+}
